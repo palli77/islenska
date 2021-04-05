71ddfb608f6312b6b63d5e533f5eaabb04064fca
Páll Baldursson <pb1@nemandi.ntv.is>
2021-04-05 12:28:12 +0000

    Initial commit

---
+++ bin/Íslenska.dart
@@ -0,0 +1,20 @@
+void main(list<String>arguments) {
+ bool answer = true;
+
+ if(answer) {
+
+   print('inside if statement');
+
+ }
+ }
+print('Rússland er stærsta land heims.');
+
+if ('Rússland er stærsta land heims.' ==
+'Russia is the world´s biggest country.');
+
+print('Rétt!');
+}else;
+
+{
+print('Rangt!');
+}
---
+++ test/Íslenska_test.dart
@@ -0,0 +1,8 @@
+import 'package:Íslenska/Íslenska.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
